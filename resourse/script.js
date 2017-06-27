5c5e83cb3f5376359500b39ef993d670713feb53
sergey120182 <sergey120182@mail.ru>
2017-06-28 00:44:39 +0300

    Replacement background

--- resourse/script.js
+++ resourse/script.js
@@ -1,3 +1,32 @@
+$(document).ready(function(){
+// Replacement background
+var imgHead = ['resourse/img/webdeveloper.jpg',
+               'resourse/img/hero11.jpg'], i=0;
+function csaHead(){
+            if(i > (imgHead.length-1)){
+                  $('.top').animate({'opacity':'1'},200,function(){
+            	    i=1;
+                  $('.top').css({
+                    'background':'url('+imgHead[0]+')',
+                    'background-size':'cover',
+                    'height': '100vh',
+                    'background-position': 'center',
+                    'background-attachment': 'fixed'});});}
+            else{
+                  $('.top').animate({'opacity':'1'},200,function(){
+                  $('.top').css({
+                    'background':'url('+imgHead[i]+')',
+                    'background-size':'cover',
+                    'height': '100vh',
+                    'background-position': 'center',
+                    'background-attachment': 'fixed'
+                });
+                  i++;
+           });
+      }
+  }
+var intervalCsaHead = setInterval(csaHead,8000);
+}
 
 
   
